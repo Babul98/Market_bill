244f8ea6177035e8cd159019d50bce5a08a04bb6
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-10-19 20:28:59 +0530

    colors and fixed bug in printBill

--- Market_billing.h
+++ Market_billing.h
@@ -8,17 +8,24 @@
 #include <string>
 #include <thread>
 
-// Removing windows dependency for sleep and using threads
-// for better waiting of process on any machine
-
 using namespace std;
 
-// Macro for clearing screen depending on system
+// Clear Macro
 #ifdef _WIN32
 #define CLEAR "cls"
 #else
 #define CLEAR "clear"
 #endif
+
+// Color Codes
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
 
 class Bill {
 private:
